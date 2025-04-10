cce03e2f30418c88a8b6d9b50774b4287316405f
giorumy <giorumy@gmail.com>
2025-04-10 18:28:44 -0400

    setting up

--- data.cpp
+++ data.cpp
@@ -1,3 +1,1 @@
-#include <iostream>
-
 //this should be the file where we parse through the api
--- data.h
+++ data.h
@@ -2,18 +2,6 @@
 #define DATA_H
 
 #include <iostream>
-#include <vector>
-#include <string>
 using namespace std;
-
-//I am not sure what would be the best way of representing the actors and movies.
-//We could do the map<name, index> way, the vector<Actor>, or any other that you guys think would work best
-struct Actor {
-    string name;
-    string picture;
-    vector<string> movies; //maybe? then go through these movies or something idk
-};
-
-
 
 #endif //DATA_H
