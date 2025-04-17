00077c2e1556effb25ad619f64ad8b0a6099667e
giorumy <giorumy@gmail.com>
2025-04-17 02:53:24 -0400

    setting up

--- data.h
+++ data.h
@@ -2,6 +2,51 @@
 #define DATA_H
 
 #include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <sstream>
+#include "json.hpp"
+
+using json = nlohmann::json;
 using namespace std;
+
+struct Movie {
+    int id;
+    string title;
+    string release_date;
+
+    Movie(int id, string title, string release_date)
+        : id(id), title(title), release_date(release_date) {}
+};
+
+// Actor structure to store actor information
+struct Actor {
+    int id;
+    string name;
+    string profile_path;
+
+    Actor() : id(0) {}
+    Actor(int id, string name, string profile_path = "")
+        : id(id), name(name), profile_path(profile_path) {}
+};
+
+class Data {
+private:
+    string api_key;
+
+    //helper function: encodes spaces in actor names for the URL
+    //e.g. "Tom Hanks" -> "Tom%20Hanks"
+    string urlEncode(string& str);
+public:
+    //constructor
+    Data(string key) : api_key(key){}
+
+    vector<Movie> getMovies(string actor_name); //returns a list of movies that have a given actor
+    vector<Actor> getActors(string movie_name); //returns a list of actors in a given movie
+
+    //TODO: the functions below are for if we end up having time for front end stuff.
+    string getActorImage(string actorName); //returns url of actor image
+    string getMovieImage(string movieName); //returns url of movie poster image
+};
 
 #endif //DATA_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include "data.h"
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
-
     std::cout << ":)" << std::endl;
+
+    Data data("07663db07b6982f498aef71b6b0997f7");
+
+    //example of API use:
+
+    //with an actor:
+    string actor = "Tom Hanks";
+    vector<Movie> movies = data.getMovies(actor);
+    cout << "Movies featuring " << actor << ":\n";
+    for (const auto& movie : movies) {
+        cout << "- " << movie.title << " (" << movie.release_date << "), ID: " << movie.id << "\n";
+    }
+
+    cout << "----------------------------------" << endl;
+
+    //with a movie:
+    string movie = "Forrest Gump";
+    vector<Actor> actors = data.getActors(movie);
+    cout << "Actors featuring " << movie << ":\n";
+    for (const auto& actor : actors) {
+        cout << "- " << actor.name << " (" << actor.id << "), Pic: " << actor.profile_path << "\n";
+    }
+
+
     return 0;
 }
